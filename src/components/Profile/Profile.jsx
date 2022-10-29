b439c6a6b2e956cba3869a65221264b2cb061219
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-10-29 22:05:00 +0100

    fix: linktree

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <main>
+      <section className="image-name">
+        <img src="" alt="" />
+        <p className="name"></p>
+      </section>
+      <section className="links">
+        <div className="link"></div>
+      </section>
+    </main>
+  );
+}
+
+export default Profile;
