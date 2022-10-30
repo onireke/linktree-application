8f9bbf7d2edd88c8c37b7e957807326afb313e84
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-10-30 04:55:30 +0100

    fix: linktree

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Icons.scss";
 
 function Icons() {
   return (
     <div className="icons">
-      <img src="" alt="" className="icon" />
-      <img src="" alt="" className="icon" />
+      <img src="/images/slack.png" alt="slack_img" className="icon" />
+      <img src="/images/github" alt="github_icon" className="icon" />
     </div>
   );
 }
