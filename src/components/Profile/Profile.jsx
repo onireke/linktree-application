6026796d9f0ed367dfbb932cf6404fc19dd87c7b
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-10-30 04:38:13 +0100

    fix: linktree

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -13,10 +13,44 @@ function Profile() {
         <p className="name" id="twitter">
           Amosa Abdulfatah Olayinka
         </p>
+
+        {/* <p className="name" id="slack">
+          Amosa Abdulfatah Olayinka
+        </p> */}
       </section>
       <section className="links">
-        <a href="">
-          <div className="link"></div>
+        <a href="https://twitter.com/BlessedCoder">
+          <btn className="link">Twitter Link</btn>
+        </a>
+
+        <a href="https://training.zuri.team/">
+          <btn className="link" id="btn_zuri">
+            Zuri Team
+          </btn>
+        </a>
+
+        <a href="http://books.zuri.team/">
+          <btn className="link" id="books">
+            Zuri Team
+          </btn>
+        </a>
+
+        <a href="https://books.zuri.team/">
+          <btn className="link" id="book_python">
+            Python Books
+          </btn>
+        </a>
+
+        <a href="https://background.zuri.team/">
+          <btn className="link" id="pitch">
+            Background Check for Coders
+          </btn>
+        </a>
+
+        <a href="https://background.zuri.team/">
+          <btn className="link" id="book_design">
+            Design Book
+          </btn>
         </a>
       </section>
     </main>
