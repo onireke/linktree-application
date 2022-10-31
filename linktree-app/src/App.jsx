9af23f9550bdc09c0ae310de2694abca0570f43b
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-10-31 23:24:19 +0100

    fix: linktree-app

---
+++ linktree-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Icons from "./components/Icons/Icons";
+import Profile from "./components/Profile/Profile";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Icons />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
